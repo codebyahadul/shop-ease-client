b2752913e260e28681af90cc2c226bbb991df937
codebyahadul <ahadul.prof@gmail.com>
2024-08-16 11:12:52 +0600

    aded sorting by price and added date

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,64 +1,94 @@
 import Card from "../components/shared/Card";
 import FilterSidebar from "../components/FilterSidebar";
-import useGetProducts from "../hooks/useGetProducts";
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const Home = () => {
-    // const [products] = useGetProducts()
     const [toggle, setToggle] = useState(false)
 
     // pagination related
     const {count} = useLoaderData();
-    console.log(count);
-    
     const [currentPage, setCurrentPage] = useState(0)
+    const [sortField, setSortField] = useState('price');
+    const [sortOrder, setSortOrder] = useState('asc');
     const page = 10
     const numberOfPage = Math.ceil(count / page)
     const  pageNumber= [...Array(numberOfPage).keys()]
+
+    // pagination & sorting
     const { data: products = [], isLoading, refetch } = useQuery({
-        queryKey: ['products', currentPage],
+        queryKey: ['products', currentPage, sortField, sortOrder],
         queryFn: async () => {
-            const { data } = await axios.get(`http://localhost:5000/products?page=${currentPage}&size=${page}`)
-            return data
+            const { data } = await axios.get(`http://localhost:5000/products?page=${currentPage}&size=${page}&sortField=${sortField}&sortOrder=${sortOrder}`);
+            return data;
         }
-    })
-    console.log(products);
-    
+    });
     const updateCurrentPage = (num) => {
         if ((num > (page - 1)) || (0 > num)) { return setCurrentPage(0) }
         setCurrentPage(num)
-        refetch()
+    }
+    if(isLoading){
+        return <div>loading</div>
     }
     return (
         <div className="px-2 my-5 md:my-8">
-            <div className="text-center my-3 pb-5 flex">
-                <input type="text" className="border-y border-l focus:outline-none px-2 md:px-5 py-1 md:py-2 bg-gray-200 font-semibold text-black rounded-s-md text-sm md:text-lg" placeholder="Search your product" />
-                <button className="border-y rounded-r-md py-1 md:py-2 px-1 md:px-4 font-semibold text-black bg-gray-400/40 hover:bg-secondary/80 text-sm md:text-lg">Search</button>
-            </div>
+            <form  className="text-center my-3 pb-5 flex">
+                <input type="text" name="name" className="border-y border-l focus:outline-none px-2 md:px-5 py-1 md:py-2 bg-gray-200 font-semibold text-black rounded-s-md text-sm md:text-lg" placeholder="Search your product" />
+                <button type="submit" className="border-y rounded-r-md py-1 md:py-2 px-1 md:px-4 font-semibold text-black bg-gray-400/40 hover:bg-secondary/80 text-sm md:text-lg">Search</button>
+            </form>
             <div className=" flex flex-col md:flex-row gap-5 md:gap-10 p-5 border rounded-lg shadow-md">
                 <FilterSidebar toggle={toggle}/>
                 <div className="block md:hidden">
                     <h1 onClick={() => setToggle(!toggle)}>Filter</h1>
                 </div>
                 <div>
-                    <div className="flex flex-col md:flex-row justify-end gap-2 items-center mb-4 text-xs md:text-lg font-medium border-b pb-4">
-                        <span >Sort By: </span>
-                        <select className="border rounded-md font-normal">
+                <div className="flex flex-col md:flex-row justify-end gap-2 items-center mb-4 text-xs md:text-lg font-medium border-b pb-4">
+                        <span>Sort By: </span>
+                        <select 
+                            className="border rounded-md font-normal"
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                if (value === 'low') {
+                                    setSortField('price');
+                                    setSortOrder('asc');
+                                } else if (value === 'high') {
+                                    setSortField('price');
+                                    setSortOrder('desc');
+                                }
+                                // setCurrentPage(0);
+                                refetch();
+                            }}
+                        >
                             <option value="default">Price</option>
-                            <option value="low"> Price Low to High</option>
-                            <option value="low"> Price High to Low</option>
+                            <option value="low">Price Low to High</option>
+                            <option value="high">Price High to Low</option>
                         </select>
-                        <select className="border rounded-md font-normal">
+                        <select 
+                            className="border rounded-md font-normal"
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                if (value === 'newest') {
+                                    setSortField('createdAt');
+                                    setSortOrder('desc');
+                                } else {
+                                    setSortField('createdAt');
+                                    setSortOrder('asc');
+                                }
+                                // setCurrentPage(0);
+                                refetch();
+                            }}
+                        >
                             <option value="default">Date Added</option>
-                            <option value="low">Newest first</option>
+                            <option value="newest">Newest First</option>
+                            <option value="oldest">Oldest First</option>
                         </select>
                     </div>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center w-full">
                         {
-                            products.map((product) => <Card key={product?._id} product={product} />)
+                           products.map((product) => <Card key={product?._id} product={product} />)
                         }
                     </div>
                 </div>
